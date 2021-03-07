074d8f7dacdab45033f28beb19e8ad7029415d28
filim <filimonbogdan89@gmail.com>
2021-03-07 20:45:42 +0200

    DbConnect+TestDB

--- svc/CineM8/CineM8/Controllers/UserController.cs
+++ svc/CineM8/CineM8/Controllers/UserController.cs
@@ -1,20 +1,29 @@
-﻿using CineM8.Models;
+﻿using CineM8.DAL;
+using CineM8.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-
+using System.Diagnostics;
 
 namespace CineM8.Controllers
 {
     public class UserController : ApiController
     {
+        DBConnect dBConnect;
+        UserDAL userDAL;
         [HttpGet]
         public IEnumerable<string> GetAllUsers()
         {
-            return new string[] { "toni", "caia","exemplu" };
+            List<User> users = new List<User>();
+            userDAL = new UserDAL();
+            dBConnect = new DBConnect();
+            dBConnect.OpenConnection();
+            users = userDAL.GetAllUsers();
+            dBConnect.CloseConnection();
+            return new string[] { users[0].FirstName, users[0].Id.ToString()};
         }
     }
 }
---
+++ svc/CineM8/CineM8/DAL/UserDAL.cs
@@ -0,0 +1,34 @@
+﻿using CineM8.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace CineM8.DAL
+{
+    public class UserDAL
+    {
+        public List<User> GetAllUsers()
+        {
+            List<User> users = new List<User>();
+            string query = "SELECT * FROM Student;";
+            MySqlDataAdapter da = new MySqlDataAdapter(query, DBConnect.conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Student");
+            DataTable dt = ds.Tables["Student"];
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                User user = new User();
+                user.FirstName = dr["name"].ToString();
+                user.Id = Convert.ToInt32(dr["studentID"]);
+
+                users.Add(user);
+            }
+
+            return users;
+        }
+    }
+}
