6bc18f4fa2ede53f654a2a308693f85026784f26
filim <filimonbogdan89@gmail.com>
2021-03-07 22:15:39 +0200

    Reservation+Movie-classes-changed

--- svc/CineM8/CineM8/Models/Movie.cs
+++ svc/CineM8/CineM8/Models/Movie.cs
@@ -13,13 +13,42 @@ namespace CineM8.Models
         private float length;
         private bool isRunning;
 
-        public Movie()
+        public Movie(string name, string description, float length, bool isRunning)
         {
-            this.name = "Shrek";
-            this.description = "ok";
+            this.name = name;
+            this.description = description;
+            this.length = length;
+            this.isRunning = isRunning;
         }
+    
         //TODO : more fields such as length, actor, rating... construtors, getters and setters
+        // DONE : constructors, getters and setters
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
 
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
 
+        public string Description
+        {
+            get => description;
+            set => description = value;
+        }
+        public float Length
+        {
+            get => length;
+            set => length = value;
+        }
+        public bool IsRunning
+        {
+            get => isRunning;
+            set => isRunning = value;
+        }
     }
 }
--- svc/CineM8/CineM8/Models/Reservation.cs
+++ svc/CineM8/CineM8/Models/Reservation.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace CineM8.Models
 {
@@ -14,7 +11,60 @@ namespace CineM8.Models
         private int numberOfSeats;
         private DateTime startTime;
         private DateTime endTime;
+
         //TODO constructors, getters and setters
+        // DONE : constructors, getters and setters
+
+        public Reservation(int userId, int movieId, int cinemaHallId, int numberOfSeats, DateTime startTime, DateTime endTime)
+        {
+            this.userId = userId;
+            this.cinemaHallId = cinemaHallId;
+            this.numberOfSeats = numberOfSeats;
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public int UserId
+        {
+            get => userId;
+            set => userId = value;
+        }
+        
+        public int MovieId
+        {
+            get => movieId;
+            set => movieId = value;
+        }
+
+        public int CinemaHallId
+        {
+            get => cinemaHallId;
+            set => cinemaHallId = value;
+        }
+
+        public int NumberOfSeats
+        {
+            get => numberOfSeats;
+            set => numberOfSeats = value;
+        }
+
+        public DateTime StartTime
+        {
+            get => startTime;
+            set => startTime = value;
+        }
+
+        public DateTime EndTime
+        {
+            get => endTime;
+            set => endTime = value;
+        }
 
     }
 }
