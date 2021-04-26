526eda262d897356a75fc6384ff054c31bf53a11
filim <filimonbogdan89@gmail.com>
2021-04-26 12:12:21 +0300

    ReservationDAL, Reservation Controller (CRUD)

---
+++ svc/CineM8/CineM8/Controllers/ReservationController.cs
@@ -0,0 +1,76 @@
+﻿using CineM8.DAL;
+using CineM8.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System;
+
+namespace CineM8.Controllers
+{
+    [RoutePrefix("api/reservation")]
+    public class ReservationController : ApiController
+    {
+        static DBConnect dBConnect = new DBConnect();
+        ReservationDAL reservationDAL = new ReservationDAL();
+        [HttpGet]
+        [Route("GetReservation")]
+
+        public JsonResult<List<Reservation>> GetAllMovies()
+        {
+
+            List<Reservation> reservations = new List<Reservation>();
+            dBConnect.OpenConnection();
+            reservations = reservationDAL.GetAllReservations();
+            dBConnect.CloseConnection();
+            return Json<List<Reservation>>(reservations);
+        }
+
+        [HttpGet]
+        [Route("ReadReservation/{reservationId}")]
+        public JsonResult<List<Reservation>> ReadMovie(int reservationId)
+        {
+            dBConnect.OpenConnection();
+            List<Reservation> reservations = new List<Reservation>();
+            reservations = reservationDAL.readReservation(reservationId);
+            dBConnect.CloseConnection();
+            return Json<List<Reservation>>(reservations);
+        }
+
+        [HttpPost]
+        [Route("PostNewReservation")]
+        public JsonResult<string> PostReservation(Reservation reservation)
+        {
+            dBConnect.OpenConnection();
+
+            reservationDAL.createReservation(reservation);
+            string comment = "Reservation created succesfully!";
+            dBConnect.CloseConnection();
+
+            return Json<string>(comment);
+        }
+
+        [HttpDelete]
+        [Route("DeleteReservation/{reservationId}")]
+        public JsonResult<string> DeleteReservation(int reservationId)
+        {
+            dBConnect.OpenConnection();
+
+            reservationDAL.deleteReservation(reservationId);
+            string comment = "Reservation Deleted Succesfully!";
+            dBConnect.CloseConnection();
+            return Json<string>(comment);
+        }
+
+        [HttpPut]
+        [Route("UpdateReservation/{reservationId}")]
+        public JsonResult<string> UpdateReservation(int reservationId, Reservation reservation)
+        {
+            dBConnect.OpenConnection();
+            reservationDAL.updateReservation(reservationId, reservation);
+            string message = "Movie Updated Succesfully!";
+            dBConnect.CloseConnection();
+            return Json<string>(message);
+        }
+
+    }
+}
---
+++ svc/CineM8/CineM8/DAL/ReservationDAL.cs
@@ -0,0 +1,166 @@
+﻿using CineM8.Models;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+
+
+namespace CineM8.DAL
+{
+    public class ReservationDAL
+    {
+        public List<Reservation> GetAllReservations()
+        {
+            try
+            {
+                List<Reservation> reservations = new List<Reservation>();
+                DataSet ds;
+                MySqlDataAdapter da;
+
+                using (MySqlConnection connection = new MySqlConnection(DBConnect.conString))
+                {
+                    string query = "SELECT * FROM Reservation;";
+                    connection.Open();
+
+                    da = new MySqlDataAdapter(query, connection);
+                    ds = new DataSet();
+                    da.Fill(ds, "Reservation");
+                }
+                DataTable dt = ds.Tables["Reservation"];
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    int userID = Convert.ToInt32(dr["userID"]);
+                    int movieID = Convert.ToInt32(dr["movieID"]);
+                    int hallID = Convert.ToInt32(dr["hallID"]);
+                    int numberOfSeats = Convert.ToInt32(dr["numberOfSeats"]);
+                    DateTime startTime = Convert.ToDateTime(dr["startTime"]);
+                    DateTime endTime = Convert.ToDateTime(dr["endTime"]);
+                    Reservation reservation = new Reservation(userID, movieID, hallID, numberOfSeats, startTime, endTime);
+
+                    reservations.Add(reservation);
+                }
+
+                return reservations;
+
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return null;
+        }
+
+        public void createReservation(Reservation reservation)
+        {
+            using (MySqlConnection connection = new MySqlConnection(DBConnect.conString))
+            {
+                MySqlCommand comm = DBConnect.conn.CreateCommand();
+                comm.CommandText = "INSERT INTO Reservation(userID, movieID, hallID, numberOfSeats, startTime, endTime) " +
+                    "VALUES(@userID, @movieID, @hallID, @numberOfSeats, @startTime, @endTime)";
+                comm.Parameters.AddWithValue("@userID", reservation.UserId);
+                comm.Parameters.AddWithValue("@movieID", reservation.MovieId);
+                comm.Parameters.AddWithValue("@hallID", reservation.CinemaHallId);
+                comm.Parameters.AddWithValue("@numberOfSeats", reservation.NumberOfSeats);
+                comm.Parameters.AddWithValue("@startTime", reservation.StartTime);
+                comm.Parameters.AddWithValue("@endTime", reservation.EndTime);
+                try
+                {
+                    comm.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: {0}", e.ToString());
+                }
+            }
+        }
+
+        public List<Reservation> readReservation(int id)
+        {
+            try
+            {
+                List<Reservation> reservations = new List<Reservation>();
+                DataSet ds;
+                MySqlDataAdapter da;
+
+                using (MySqlConnection connection = new MySqlConnection(DBConnect.conString))
+                {
+                    string query = "SELECT * FROM Reservation where reservationID = " + id;
+                    connection.Open();
+
+                    da = new MySqlDataAdapter(query, connection);
+                    ds = new DataSet();
+                    da.Fill(ds, "Reservation");
+                }
+                DataTable dt = ds.Tables["Reservation"];
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    int userID = Convert.ToInt32(dr["userID"]);
+                    int movieID = Convert.ToInt32(dr["movieID"]);
+                    int hallID = Convert.ToInt32(dr["hallID"]);
+                    int numberOfSeats = Convert.ToInt32(dr["numberOfSeats"]);
+                    DateTime startTime = Convert.ToDateTime(dr["startTime"]);
+                    DateTime endTime = Convert.ToDateTime(dr["endTime"]);
+                    Reservation reservation = new Reservation(userID, movieID, hallID, numberOfSeats, startTime, endTime);
+
+                    reservations.Add(reservation);
+                }
+
+                return reservations;
+
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return null;
+        }
+
+        public void updateReservation(int id, Reservation reservation)
+        {
+            using (MySqlConnection connection = new MySqlConnection(DBConnect.conString))
+            {
+                MySqlCommand comm = DBConnect.conn.CreateCommand();
+                comm.CommandText = "update Reservation set userID = @userID , " +
+                    "movieID = @movieID, hallID = @hallID, numberOfSeats = @numberOfSeats, startTime = @startTime, " +
+                    "endTime = @endTime where reservationID = @id";
+                comm.Parameters.AddWithValue("@userID", reservation.UserId);
+                comm.Parameters.AddWithValue("@movieID", reservation.MovieId);
+                comm.Parameters.AddWithValue("@hallID", reservation.CinemaHallId);
+                comm.Parameters.AddWithValue("@numberOfSeats", reservation.NumberOfSeats);
+                comm.Parameters.AddWithValue("@startTime", reservation.StartTime);
+                comm.Parameters.AddWithValue("@endTime", reservation.EndTime);
+                try
+                {
+                    comm.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: {0}", e.ToString());
+                }
+            }
+        }
+        public void deleteReservation(int id)
+        {
+            using (MySqlConnection connection = new MySqlConnection(DBConnect.conString))
+            {
+                MySqlCommand comm = DBConnect.conn.CreateCommand();
+                comm.CommandText = "DELETE from Reservation where reservationID = @id";
+                comm.Parameters.AddWithValue("@id", id);
+
+                try
+                {
+                    comm.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: {0}", e.ToString());
+                }
+            }
+
+        }
+        }
+    }
+}
