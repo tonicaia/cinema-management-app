81ada4f319031fd2f21cc0e1427cf626f0e45a0c
Botar Vlad <vlad.botar99@yahoo.com>
2021-05-09 16:18:06 +0300

    seats render by script

--- svc/CineM8/CineM8/Scripts/movie.js
+++ svc/CineM8/CineM8/Scripts/movie.js
@@ -2,8 +2,69 @@ const container = document.querySelector('.container2');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const submitButton = document.getElementById('submitButton');
+
+let allSeats = [];
+let seatsBits = [];
+
+const places = document.getElementById('places');
+
+function getHall() {
+  fetch('https://localhost:44300/api/halls/GetHall', {
+    method: 'Get',
+  })
+    .then(response => response.json())
+    .then(data => allSeats = data[1].Capacity)
+    .then(() => {
+      for (let i = 0; i < allSeats; i++) {
+        seatsBits.push(0);
+      }
+    })
+    .then(() => {
+      let seatIndex = 1;
+      let stopIndex = 11;
+      for (let i = 1; i <= 8; i++) {
+        places.innerHTML += `
+          <div class="row" id="row${i}">
+        `;
+        let row = document.getElementById('row' + i);
+
+        for (let i = seatIndex; i <= stopIndex; i++) {
+          row.innerHTML += `
+          <div class="seat">${i}</div>
+        `;
+          seatIndex++;
+        }
+        places.innerHTML += `
+          </div>
+        `;
+        stopIndex += 11;
+      }
+    })
+}
+
+getHall();
+
 submitButton.addEventListener("click", function () {
   console.log(seatsReserved);
+
+  //const reservation = {
+  //  userId: 1,
+  //  movieId: 1,
+  //  cinemaHallId: 1,
+  //  numberOfSeats: seatsReserved.length,
+  //  seatsNumbers: 
+  //};
+
+  // private int id;
+  //      private int userId;
+  //      private int movieId;
+  //      private int cinemaHallId;
+  //      private int numberOfSeats;
+  //      private BitArray seatsNumbers;
+  //      private DateTime startTime;
+  //      private DateTime endTime;
+
+  fetch('https://localhost:44300/api/reservation/PostNewReservation', { method: 'Post' })
 });
 
 
@@ -36,4 +97,3 @@ container.addEventListener('click', e => {
   }
   updateSelectedCount();
 });
-
